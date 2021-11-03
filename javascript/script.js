0530a3dd49a0c685fd17eb0d0de003c9648d1250
Dev-gustavo-henrique <gustavojs417@gmail.com>
2021-11-03 17:43:51 -0300

    changes refatoring scripts

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,22 @@
-document.getElementById ("botaoEnviar").addEventListener("click", enviaFormulario)
+var inputBotaoEnviar = document.getElementById("botaoEnviar");
+var inputNomeDoUsuario = document.getElementById("nomeDoUsuario");
+var inputEmailDoUsuario = document.getElementById("emailDoUsuario");
+var inputTelUsuario = document.getElementById("telUsuario");
 
-function enviaFormulario () {
+inputBotaoEnviar.addEventListener("click", enviaFormulario);
 
-  if (document.getElementById("nomeDoUsuario").value != "" && document.getElementById("emailDoUsuario").value != "" && document.getElementById("telUsuario").value != "") {
-    alert ("Pronto! Você receberá as novidades por email!")
+window.onscroll = () =>{
+  if(window.scrollY > 500){
+      document.querySelector('#scroll-top').classList.add('active');
+  }else{
+      document.querySelector('#scroll-top').classList.remove('active');
+  }
+}
+
+function enviaFormulario() {
+  if (inputNomeDoUsuario.value != "" && inputEmailDoUsuario.value != "" && inputTelUsuario.value != "") {
+    alert ("Pronto! Você receberá as novidades por email!");
   } else {
-    alert ("Por favor, preencha todos os campos")
+    alert ("Por favor, preencha todos os campos");
   }
 }
